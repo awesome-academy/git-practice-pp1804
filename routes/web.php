c5d059147924884e5e7d1341ba8ab27fb9d2b161
kenini1805 <huusu1996@gmail.com>
2018-11-14 19:59:21 +0700

    commit-four-branch

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 
+
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
 
 
 Route::get('/home', 'HomeController@index')->name('home');
